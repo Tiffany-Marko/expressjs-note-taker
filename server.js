43d4e23937fb1ffc918137729c5f13456d2cd4cb
Tiffany Marko <tiffanyjmarko@gmail.com>
2022-10-28 07:41:31 -0700

    update folders

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.post('/api/notes/',(req,res)=>{
         }
     })
 })
-app.get('*',(req,res)=>{
+app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"./public/notes.html"))
 })
 app.get('/notes',(req,res)=>{
